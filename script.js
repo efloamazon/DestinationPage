d7826254cbf2988456121faa1f6e006755e2799c
Eric.Flores <eflo@amazon.com>
2022-07-17 23:14:01 -0700

    start js

---
+++ script.js
@@ -0,0 +1,9 @@
+const submitBtn = document.getElementById("submit_btn")
+submitBtn.addEventListener("click", (evtObj) => {
+    evtObj.preventDefault();
+})
+
+const destinationVal = document.getElementById("name").value;
+const locationVal = document.getElementById("location").value;
+const photoVal = document.getElementById("photo").value;
+const descriptionVal = document.getElementById("description").value;
